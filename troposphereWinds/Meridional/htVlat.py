3e01826a7e551d2494f972192cd873b5bbb689da
Mihir-DG <47943744+Mihir-DG@users.noreply.github.com>
2020-07-24 10:11:12 +0530

    Found errors with code- to be altered and rerun

--- troposphereWinds/Meridional/htVlat.py
+++ troposphereWinds/Meridional/htVlat.py
@@ -20,15 +20,15 @@ for level in levels:
 p850,p250 = profiles[0],profiles[1]
 p850,p250 = array(slice_per(p850,73)),array(slice_per(p250,73))
 profile850, profile250 = [],[]
-for elem in range(len(p850)):
-	p8 = p850[elem]
+for elem in range(len(p850)): #for elem in p850:
+	p8 = p850[elem]		# Correct for 24-27: profile850 = np.mean(p850,axis=0)
 	p8 = mean(p8)
 	profile850.append(p8)
 	p2 = p250[elem]
 	p2 = mean(p2)
 	profile250.append(p2)
 
-x = nc.variables['lat'][::-1]
+x = nc.variables['lat'][::-1] # REVERSE!!!!!!
 plt.plot(x, profile250,label='250')
 plt.plot(x, profile850,label='850')
 plt.xlabel("Latitude (°)")
