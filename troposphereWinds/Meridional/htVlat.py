926ba140c3ffaa494af1d7d264bb02ac6ebaaea5
Mihir-DG <47943744+Mihir-DG@users.noreply.github.com>
2020-09-15 16:42:42 +0530

    Update htVlat.py
    Cleaned up unnecessary comments. Prepped for presentation as eg.

--- troposphereWinds/Meridional/htVlat.py
+++ troposphereWinds/Meridional/htVlat.py
@@ -23,16 +23,7 @@ p850,p250 = array(slice_per(p850,73)),array(slice_per(p250,73))
 profile850, profile250 = [],[]
 p850 = mean(p850,axis=1)
 p250 = mean(p250,axis=1)
-"""
-for elem in range(len(p850)): #for elem in p850:
-	p8 = p850[elem]		# Correct for 24-27: profile850 = np.mean(p850,axis=0)
-	p8 = mean(p8)
-	profile850.append(p8)
-	p2 = p250[elem]
-	p2 = mean(p2)
-	profile250.append(p2)
-"""
-x = nc.variables['lat'] # REVERSE!!!!!!
+x = nc.variables['lat']
 plt.plot(x, p250,label='250')
 plt.plot(x, p850,label='850')
 plt.axhline(0,color='black',alpha=0.4)
